12eb8401e901912da7ab6da31ae7601aad4db2c9
Jesse Wilson <staypuff64@gmail.com>
2024-03-24 17:24:51 -0400

    Fixed code, took out user input for shift key

--- cipher.py
+++ cipher.py
@@ -1,14 +1,15 @@
 abc = "abcdefghijklmnopqrstuvwxyz"
-message = input("Enter a message to encrypt:")
+message = input("Enter a sentence:")
 message = message.lower()
-#Get shift key
-while True:
-    key = input("Enter a shift number:")
-    if key.isdecimal():
-        key = int(key)
-        if key in range(26):
-            break
-        print("You need to enter a number between 0 and 25.")
+key = 5
+#Get shift key from user comented out
+#while True:
+    #key = input("Enter a shift number:")
+    #if key.isdecimal():
+       #key = int(key)
+        #if key in range(26):
+            #break
+        #print("You need to enter a number between 0 and 25.")
 #Find the letters
 encrypted_char = ""
 for char in message:
@@ -18,4 +19,4 @@ for char in message:
         char = abc[index]
     encrypted_char += char
 
-print(encrypted_char)
+print("The encrypted sentence is:", encrypted_char)
